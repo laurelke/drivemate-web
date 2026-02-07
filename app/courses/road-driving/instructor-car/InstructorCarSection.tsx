103624fb35d805a54e5c964d09e7171794c70c67
Laurel Ke <laurel@LaureldeMacBook-Air.local>
2026-02-07 22:15:39 +0800

    feat: unify course pages layout and design

--- app/courses/road-driving/instructor-car/InstructorCarSection.tsx
+++ app/courses/road-driving/instructor-car/InstructorCarSection.tsx
@@ -38,7 +38,7 @@ export default function InstructorCarSection({
             className="
               mx-auto
               h-auto
-              w-full
+              w-[720px]
               max-w-[720px]
               max-h-[360px]
               sm:max-h-[360px]
