58f5a18127d8dc229602aad85e8c7638f0719b02
Laurel Ke <laurel@gmail.com>
2026-04-23 22:29:16 +0800

    Add canonical redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.drivemate-tw.com',
+          },
+        ],
+        destination: 'https://drivemate-tw.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
